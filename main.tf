4408e1b771880e39caf1af0530dab7db1fa5b4ba
Claudio Cordova <claudio_cordova@yahoo.com>
2023-07-21 21:53:59 -0700

    Update main.tf SG

--- main.tf
+++ main.tf
@@ -22,3 +22,43 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 }
+
+data "aws_vpc" "default" {
+  defauolt = true
+}
+
+
+
+
+resource "aws_security_group" "blog" {
+    description = "Allow http/https in. all out"
+    name = "blog"
+    tags = {
+        for-use-with-amazon-emr-managed-policies = "true"
+    }
+    vpc_id = data.aws_vpc.default.id
+
+    ingress {
+        security_groups = [aws_security_group.blog.id]
+        from_port = 80
+        protocol = "tcp"
+        to_port = 80
+        cidr_blocks = ["0.0.0.0/0"]        
+    }
+
+    ingress {
+        security_groups = [aws_security_group.blog.id]
+        from_port = 443
+        protocol = "tcp"
+        to_port = 443
+        cidr_blocks = ["0.0.0.0/0"]        
+    }
+
+    egress {
+        security_groups = [aws_security_group.blog.id]
+        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 0
+        protocol = "-1"
+        to_port = 0
+    }
+}
