3bc131682ab57f51a39f3fb007995c82d65a457a
Claudio Cordova <claudio_cordova@yahoo.com>
2023-07-22 20:33:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
   name = "blog_sg_from_module"
-  #vpc_id = data.aws_vpc.default.id
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = data.aws_vpc.default.id
+  #vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"] 
