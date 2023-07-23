466e7cbd51dec8f1caf7fd5ffff80166c5b4a8df
Claudio Cordova <claudio_cordova@yahoo.com>
2023-07-22 23:27:31 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "blog" {
 }
 
 
-resource "aws_instance" "blog2" {
+resource "aws_instance" "blog3" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   associate_public_ip_address = true
@@ -82,7 +82,24 @@ resource "aws_instance" "blog2" {
 }
 
 
-module "alb" {
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+  name = "blog"
+  min_size = 1 
+  max_size = 2
+
+  vpc_zone_indentifier = module.vpc.public_subnets
+  target_group_arns = module.blog_alb.target_groups_arns
+  security_groups = [module.blog_sg.security_group_id]
+  
+  image_id           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+}
+
+
+
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
