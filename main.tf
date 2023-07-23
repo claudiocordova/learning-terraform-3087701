d2df4194dab8af63fc503d8a7c6257c77e4489a2
Claudio Cordova <claudio_cordova@yahoo.com>
2023-07-22 20:56:34 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_sg_from_module"
   #vpc_id = data.aws_vpc.default.id
-  vpc_id = module.vpc.public_subnets[1]
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"] 
