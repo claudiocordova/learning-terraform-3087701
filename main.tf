a77740cb8d6fe02048ae500d50254f8709a1a03c
Claudio Cordova <claudio_cordova@yahoo.com>
2023-07-22 19:14:25 -0700

    Update main.tf module

--- main.tf
+++ main.tf
@@ -15,8 +15,17 @@ data "aws_ami" "app_ami" {
 }
 
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog_sg_from_module"
+  vpc_id = data.aws_vpc.default.id
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"] 
 
-
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"] 
+}
 
 
 
@@ -61,7 +70,10 @@ resource "aws_instance" "blog" {
   tags = {
     Name = "HelloWorld"
   }
-  vpc_security_group_ids = [aws_security_group.blog.id] 
+  #vpc_security_group_ids = [aws_security_group.blog.id]
+  # security_group_id from
+  # https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest?tab=outputs
+  vpc_security_group_ids = [module.blog_sg.security_group_id] 
 }
 
 data "aws_vpc" "default" {
