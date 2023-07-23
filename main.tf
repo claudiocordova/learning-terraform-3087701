af09fcde86e2695ee456863de641e0adb0f71163
Claudio Cordova <claudio_cordova@yahoo.com>
2023-07-22 23:35:08 -0700

    qqqqqq

--- main.tf
+++ main.tf
@@ -121,11 +121,11 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog2.id
-          port = 80
-        }
+      #targets = {
+      #  my_target = {
+      #    target_id = aws_instance.blog2.id
+      #    port = 80
+      #  }
 
       }
     }
